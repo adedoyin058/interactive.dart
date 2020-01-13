788e0472bd48e2215d2873d93df936df2cec33ae
olaniyi <adedoyin058@gmail.com>
2020-01-13 06:55:58 -0800

    updated interactive

--- interactive.dart
+++ interactive.dart
@@ -1,23 +1,16 @@
-import 'dart:html';
+
 import 'dart:io';
 
 
 
   main() {
-     var lst = new List(10);
-      lst [0]= 'adedoyin&co';
-      lst [0]= 'Goodtime company';
-      lst [0]= 'Boomerang';
-      lst [0]= 'Solar.ng';
-      lst [0]= 'Adedoyin.ng';
-      lst [0]= 'Ade.ng';
-
-print(lst);
-print('Please choose from the names listed above  to name your company');
-   print('supply the name you will like to name your company');
-    
+   var firstName = 'input'; 
+   print('please type your name');
   var input = stdin.readLineSync();
-   
-  print(input.toUpperCase());
+  
+
+print(input.toUpperCase());
+   print(input.length);
+  
 
 }
